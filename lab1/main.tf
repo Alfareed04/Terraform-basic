3349f6ad51455a722f9db1c6413d0a254116b52f
Alfareed04 <alfareed845@gmail.com>
2024-07-03 11:25:56 +0530

    lab1

---
+++ lab1/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"           // Create a Resource Group using Terraform
+}
+ 
+provider "azurerm" {
+    features {}
+}
+ 
+resource "azurerm_resource_group" "rg" {
+    name = "myrg"
+    location = "southeast asia"  
+}
